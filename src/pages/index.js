0eed7192f8ee5be80e06e5ac0f779854c07b260f
Ryan <ryanjamesdesigner@gmail.com>
2024-02-22 00:08:18 -0800

    fix head component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,6 @@
 import * as React from "react";
 import * as styles from "./styles.module.scss";
 import { StaticImage } from "gatsby-plugin-image";
-// import Logo from "../images/rnbass-logo-black.png";
-
-// const buttonStyles = {
-//   padding: "1rem 3rem",
-//   borderRadius: 4,
-//   fontSize: "1.25rem",
-//   color: "#efefef",
-
-//   letterSpacing: 1.5,
-//   textTransform: "uppercase",
-//   cursor: "pointer",
-//   background: "linear-gradient(35deg, #111111, firebrick)",
-//   border: "1px solid firebrick"
-// };
 
 const IndexPage = () => {
   return (
@@ -44,4 +30,12 @@ const IndexPage = () => {
 
 export default IndexPage;
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => {
+  return (
+    <>
+      <title>RnBass</title>
+      <meta name="title" content="RnBass" />
+      <meta name="description" content="rnbass.com" />
+    </>
+  );
+};
