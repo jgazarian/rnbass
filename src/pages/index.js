932dc2d48e4b466e733707556ff9b5474e235dfb
Ryan <ryanjamesdesigner@gmail.com>
2024-02-22 00:45:03 -0800

    add dark mode logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,46 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import * as styles from "./styles.module.scss";
 import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        setIsDark(event.matches);
+      });
+  }, []);
+
   return (
-    <main className={styles.realHero}>
-      <div>
+    <main className={styles.hero}>
+      {isDark ? (
         <StaticImage
           className={styles.logo}
-          src={`../images/rnbass-logo-black.png`}
+          src={`../images/rnbass-logo-white.png`}
           alt="the rnbass logo"
           placeholder="blurred"
         />
-        {/* <a
+      ) : (
+        <StaticImage
+          src={`../images/rnbass-logo-black.png`}
+          className={styles.logo}
+          alt="the rnbass logo"
+          placeholder="blurred"
+        />
+      )}
+      {/* <a
           style={{ color: "#f8f8f8" }}
           href="https://buy.stripe.com/test_6oE2939ZV0lpfbGaEE"
         >
           Buy Social Media Promotion
         </a> */}
-        {/* <form method="POST" action="/api/checkout">
+      {/* <form method="POST" action="/api/checkout">
           <button style={buttonStyles} type="submit">
             PURCHASE
           </button>
         </form> */}
-      </div>
     </main>
   );
 };
